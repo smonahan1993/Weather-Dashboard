e20330a18759c6641bbd2107b3d9d22a729914db
Sean Monahan <sean.r.monahan3@gmail.com>
2021-08-07 08:40:45 -0400

    added fetch

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,16 @@ form.addEventListener("submit", e => {
   e.preventDefault();
   const inputVal = input.value;
 });
+
+const apiKey = "895b542298f393087953e11563f64657";
+const inputVal = input.value;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // do stuff with the data
+  })
+  .catch(() => {
+    msg.textContent = "Please search for a valid city 😩";
+  });
