7e81756be09c76866cd3d49d74ba5a2812baf482
Sean Monahan <sean.r.monahan3@gmail.com>
2021-08-08 20:39:49 -0400

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,54 @@
-var form = document.querySelector("#search-city");
+var searchButton = $("#search-button");
+console.log(searchButton);
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const inputVal = input.value;
-});
+// var apiKey = "b8ecb570e32c2e5042581abd004b71bb";
 
+// // Forloop for persisting the data onto HMTL page
+// for (var i = 0; i < localStorage.length; i++) {
+
+//     var city = localStorage.getItem(i);
+//     // console.log(localStorage.getItem("City"));
+//     var cityName = $(".list-group").addClass("list-group-item");
+
+//     cityName.append("<li>" + city + "</li>");
+// }
+
+var temp = $("#temperature");
+var form = $("#search-city");
+var input = $("#myInput");
 const apiKey = "895b542298f393087953e11563f64657";
-const inputVal = input.value;
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
-
-fetch(url)
+// // const inputVal = form.value;
+// // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+console.log(form);
+searchButton.click( e => {
+  e.preventDefault();
+  const inputVal = input.val();
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+  fetch(url)
   .then(response => response.json())
   .then(data => {
     // do stuff with the data
+    console.log(data)
+    console.log(data.name);
+    console.log(data.main.temp *9/5 + 32);
+    temp.html(data.main.temp *9/5 + 32);
+
   })
   .catch(() => {
     msg.textContent = "Please search for a valid city 😩";
   });
+
+});
+
+// const apiKey = "895b542298f393087953e11563f64657";
+// const inputVal = form.value;
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=895b542298f393087953e11563f64657&units=metric`;
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(data => {
+//     // do stuff with the data
+//   })
+//   .catch(() => {
+//     msg.textContent = "Please search for a valid city 😩";
+//   });
