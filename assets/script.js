291bfe42844d23fafa3e781cd40f59f1b5751bdb
Sean Monahan <sean.r.monahan3@gmail.com>
2021-08-11 19:08:17 -0400

    updated js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
 console.log(searchButton);
 
 // var apiKey = "b8ecb570e32c2e5042581abd004b71bb";
@@ -12,15 +13,21 @@ console.log(searchButton);
 
 //     cityName.append("<li>" + city + "</li>");
 // }
+var historyEl = $("#history");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || []
+var today = moment();
+var cityName = $("#current-city");
 var windSpeed = $("#wind-speed");
 var uvIndex = $("#uv-index");
 var temp = $("#temperature");
 var humidity = $("#humidity")
 var form = $("#search-city");
 var input = $("#myInput");
+var lat;
+var lon;
 const apiKey = "895b542298f393087953e11563f64657";
 // // const inputVal = form.value;
-// // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+// // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&lat=units=metric`;
 console.log(form);
 searchButton.click( e => {
   e.preventDefault();
@@ -30,21 +37,77 @@ searchButton.click( e => {
   .then(response => response.json())
   .then(data => {
     // do stuff with the data
+    console.log(url);
     console.log(data)
     console.log(data.name);
-    console.log(data.main.temp *9/5 + 32);
-    temp.html(data.main.temp *9/5 + 32);
-    humidity.html(data.main.humidity);
-    windSpeed.html(data.wind.speed);
+    console.log(data.main.temp *9/5 + 32 + '\xB0 F');
+    cityName.html(data.name + ' - ' + today.format("MMM Do, YYYY"));
+    temp.html(data.main.temp *9/5 + 32 + '\xB0 F');
+    humidity.html(data.main.humidity + ' %');
+    windSpeed.html(data.wind.speed + ' MPH');
+    // uvIndex.html(data.)
 
-
+    localStorage.setItem("search",JSON.stringify(searchHistory));
+    renderSearchHistory();
 
   })
   .catch(() => {
-    msg.textContent = "Please search for a valid city 😩";
+    // msg.text("Please search for a valid city 😩");
+    input.attr("placeholder", "PLEASE ENTER A LOCATION");
+     return;
   });
 
+  var searchTerm = input.value;
+  if (!searchTerm) {
+    return;
+  }
+  //Add a conditional so that if the term is already in the history it will not push again
+  if (!searchHistory.includes(searchTerm)){ 
+      searchHistory.push(searchTerm);
+  }
+  // localStorage.setItem("search",JSON.stringify(searchHistory));
+  // renderSearchHistory();
+
 });
+
+
+// searchButton.click",function() {
+//   var searchTerm = inputEl.value;
+//   if (!searchTerm) {
+//     input.setAttribute("placeholder", "PLEASE ENTER A LOCATION");
+//     return;
+//   }
+//   //Add a conditional so that if the term is already in the history it will not push again
+//   if (!searchHistory.includes(searchTerm)){ 
+//       searchHistory.push(searchTerm);
+//   }
+//   localStorage.setItem("search",JSON.stringify(searchHistory));
+//   renderSearchHistory();
+// })
+
+function renderSearchHistory() {
+  historyEl.innerHTML = "";
+  for (let i=0; i<searchHistory.length; i++) {
+      var historyItem = document.createElement("input");
+      historyItem.setAttribute("type","text");
+      historyItem.setAttribute("readonly",true);
+      historyItem.setAttribute("value", searchHistory[i]);
+      // historyItem.addEventListener("click",function() {
+      //     (historyItem.value);
+      // var historicalLocation = (searchHistory[i]);
+      
+      // })
+      historyEl.append(historyItem);
+  }
+
+};
+
+
+
+clearButton.click(function() {
+  historyEl.empty();
+});
+
 
 // const apiKey = "895b542298f393087953e11563f64657";
 // const inputVal = form.value;
